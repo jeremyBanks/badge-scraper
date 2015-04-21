6c012c8307c27633c88d0a25c21abd56254e962a
Jeremy Banks <j@jeremybanks.ca>
2015-04-20 23:15:54 -0700

    fix test

--- election_observer_test.py
+++ election_observer_test.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 def test_get_publicist_and_have_publicist():
     so_publicist, _ = (
         election_observer.get_badge_data_and_write_function(
-            host='stackoverflow.com', badge_id=102937,
+            host='stackoverflow.com', badge_id=3109,
             filename='sheriff', require_file=True))
 
-    assert any(badge.user_id == 1114 for badge in so_publicist)
+    assert any(badge.user_id == 102937 for badge in so_publicist)
